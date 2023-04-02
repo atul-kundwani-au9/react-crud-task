168a8c0cac32f360298033cfe5817d02db4bb360
atul kundwani <atulk@tilli.pro>
2023-04-02 10:00:26 +0530

    react

--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -1,6 +1,6 @@
 const initialState = [
-  { id: 0, name: "Raman Sharma", email: "email@email.com", phone: 1234567890 },
-  { id: 1, name: "Test Name", email: "test@test.com", phone: 4567891230 },
+  { id: 0, name: "Atul", email: "email@email.com", phone: 1234567890 },
+  { id: 1, name: "Atul 1", email: "test@test.com", phone: 4567891230 },
 ];
 
 export const contactReducer = (state = initialState, action) => {
